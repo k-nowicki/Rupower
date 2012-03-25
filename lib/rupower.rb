257e5fb8da857efb0443da7a95818689a288847c
knowicki <karol@knowicki.pl>
2012-03-25 16:46:06 +0200

    consistent spaces in parentheses

--- lib/rupower.rb
+++ lib/rupower.rb
@@ -4,17 +4,17 @@ require_relative "rupower/version"
 module Rupower
 
   module Parser
-    VALUE =     Regexp.new(/(([0-9]{1,5}([.][0-9]{1,5}){0,1}).*|((\S)+))$/)
-    TIME =      Regexp.new(/\s+\([\s\w]*\)/)
-    NUMBER =    Regexp.new(/\A[+-]?\d+?(\.\d+)?\Z/)
-    UNITS =     Regexp.new(/(( .{1,2})|(%))$/)
-    AFTER_KEY = Regexp.new(/\s*:\s+[\s\S]+$/)
-    PARAGRAPH = Regexp.new(/^\s+/)
-    VALUABLE =  Regexp.new(/\w+:\s+\S+/)
+    VALUE =     Regexp.new( /(([0-9]{1,5}([.][0-9]{1,5}){0,1}).*|((\S)+))$/ )
+    TIME =      Regexp.new( /\s+\([\s\w]*\)/ )
+    NUMBER =    Regexp.new( /\A[+-]?\d+?(\.\d+)?\Z/ )
+    UNITS =     Regexp.new( /(( .{1,2})|(%))$/ )
+    AFTER_KEY = Regexp.new( /\s*:\s+[\s\S]+$/ )
+    PARAGRAPH = Regexp.new( /^\s+/ )
+    VALUABLE =  Regexp.new( /\w+:\s+\S+/ )
   end
 
   class Rupower
-    def initialize( params = {refresh_always:true})
+    def initialize( params = {refresh_always:true} )
       @refresh_always = params[:refresh_always]
       refresh
     end
@@ -24,49 +24,51 @@ module Rupower
       get_hash
     end
 
-    def method_missing(meth,*args)
-      res = get_all[sym_gsub(meth,'_','-')] || get_all[sym_gsub(meth,'_',' ')]
+    def method_missing( meth, *args )
+      name_v1 = sym_gsub( meth, '_', '-' )
+      name_v2 = sym_gsub( meth, '_', ' ' )
+      res = get_all[ name_v1 ] || get_all[ name_v2 ]
       raise NoMethodError, "undefined method #{meth} for Rupower::Battery" if res.class == NilClass
       res
     end
 
     private
-      def sym_gsub(sym, before, after)
-        sym.to_s.gsub(before,after).to_sym
+      def sym_gsub( sym, before, after )
+        sym.to_s.gsub( before, after ).to_sym
       end
 
       def get_hash
-        @state.each_line.inject({}) do |hash, line|
-          key = line.gsub(Parser::AFTER_KEY,"").gsub(Parser::PARAGRAPH,"").to_sym
-          line[Parser::VALUABLE] ? hash.merge(key => get_value( line )) : hash
+        @state.each_line.inject( {} ) do |hash, line|
+          key = line.gsub( Parser::AFTER_KEY, "" ).gsub( Parser::PARAGRAPH, "" ).to_sym
+          line[Parser::VALUABLE] ? hash.merge( key => get_value( line ) ) : hash
         end
       end
 
       def get_value( line )
-        typize( line[Parser::VALUE].gsub(Parser::UNITS,"") )
+        typize( line[Parser::VALUE].gsub( Parser::UNITS, "" ) )
       end
 
       def typize( value )
         res = value
         res = value[/^yes$/] ? true : false if value[/^(yes|no)$/]
-        res = to_number(value) if number?(value)
-        res = to_time(value) if time?(value)
+        res = to_number( value ) if number?( value )
+        res = to_time( value ) if time?( value )
         res
       end
 
-      def time?(value)
-        value.match(Parser::TIME)
+      def time?( value )
+        value.match( Parser::TIME )
       end
 
-      def to_time(value)
+      def to_time( value )
         DateTime.parse( value ).to_time
       end
 
-      def number?(value)
-        value.match(Parser::NUMBER) == nil ? false : true
+      def number?( value )
+        value.match( Parser::NUMBER ) == nil ? false : true
       end
 
-      def to_number(value)
+      def to_number( value )
         value.to_f == value.to_i ? value.to_i : value.to_f
       end
   end
@@ -90,13 +92,11 @@ module Rupower
 
   class Command
     def battery
-      device = "/org/freedesktop/UPower/devices/battery_BAT0"
-      upower(device)
+      upower( "/org/freedesktop/UPower/devices/battery_BAT0" )
     end
 
     def line_ac
-      device = "/org/freedesktop/UPower/devices/line_power_AC"
-      upower(device)
+      upower( "/org/freedesktop/UPower/devices/line_power_AC" )
     end
 
     private
